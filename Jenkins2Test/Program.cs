40b2c9f044b8dd00036fa3470f6f8b6f65ab089f
s k <szymon.kiciak@gmail.com>
2018-12-04 21:01:17 +0000

    Test

---
+++ Jenkins2Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jenkins2Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
